f0e4162599e473f68b897f8ba4ecf83c2c1c76a1
minhbro122 <Lynhan.zip.zip.zip@gmail.com>
2024-11-27 20:48:58 +0700

    Add files via upload

---
+++ SS7_BÀI1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int arr [4]= { 5,9,7,8};
+	for(int i =0;i<4;i++){
+		printf("%d \n",arr[i]); 
+	}
+	printf("do dai cua mang la %d",sizeof arr);
+	
+	
+	
+	return 0; 
+}
